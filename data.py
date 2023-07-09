3ea1b10cd735c4d70d9330f67333c2df34aa912c
Sandra Devaux <dswebprog@gmail.com>
2023-07-09 15:21:43 +0200

    extract all data

--- data.py
+++ data.py
@@ -20,6 +20,7 @@ mainhighlight = soup.find(class_="product_main")
 mainproductpage = soup.find(class_="product_page")
 mainproductgallery = soup.find("div", {"id": "product_gallery"})
 
+
 maintitlestring = mainhighlight.find("h1").string
 #pour passer des data en csv correctement, on doit en faire un tuple/liste
 maintitle = [maintitlestring]
@@ -33,54 +34,66 @@ for element in mainproductpage.select("p:nth-of-type("+str(n)+")"):
   if element not in mainhighlight:
    description.append(element.string)
 
+#position du p dans la structure class product_page
+maintable = soup.find("table")
+
+tdupc = []
+#for element_upc in maintable.select("td:nth-of-type("+str(a)+")"):
+for element_upc in maintable.select("td:nth-child(n+1)"):
+  tdupc.append(element_upc.string)
+
 # récupérer src image
 imagethumb = soup.find_all("img")
 for source in imagethumb:
-  imagesource1 = source["src"]
-  imagesource = [imagesource1]
+  imagesource = source["src"]
 #position des td dans la structure table et récupérer le contenu pour l'afficher
 #n déjà défini plus haut
 
-products = []
-def get_allinfo_table(soup, products):
-  for table in soup.find_all("table"):
-    for tr in table.find_all("tr")[1:]:
-      td = tr.find_all("td")
-      upc = td[0].get_text()
-      product_type = td[1].get_text()
-      price_excl = td[2].get_text()
-      price_incl = td[3].get_text()
-      stock = td[5].get_text()
-      reviews = td[6].get_text()
-      product = {
-        "upc": upc,
-        "product type": product_type,
-        "prix excl": price_excl,
-        "prix incl": price_incl,
-        "stock": stock,
-        "reviews": reviews
-      }
-      products.append(product)
 
 #Afficher les données dans le terminal
 print(maintitle)
 print(description)
 print(imagesource)
+print(tdupc)
 
 #envoyer en csv
 
 import csv
 
-headers = ["titre","description","image url"]
-rows = [maintitle, description, imagesource]
+headers = ["titre","description","image url","upc","category","price excl","price incl","stock","review"]
+rows = [maintitlestring, description, imagesource, tdupc]
 
 with open("Devaux_Sandra_2_data_072023.csv", "w") as fichier:
-  writer = csv.writer(fichier,delimiter=",")
+  writer = csv.writer(fichier)
   #définit la ligne d'en-têtes
   writer.writerow(headers)
 
    #définit les lignes de contenu
-  writer.writerows(rows)
+  writer.writerow(rows)
+
+
+  #essais
+
+  # products = []
+  # def get_allinfo_table(soup, products):
+  # for table in soup.find_all("table"):
+  #  for tr in table.find_all("tr")[1:]:
+  #   td = tr.find_all("td")
+  #  upc = td[0].get_text()
+  #     product_type = td[1].get_text()
+  #     price_excl = td[2].get_text()
+  #     price_incl = td[3].get_text()
+  #     stock = td[5].get_text()
+  #     reviews = td[6].get_text()
+  #     product = {
+  #       "upc": upc,
+  #       "product type": product_type,
+  #       "prix excl": price_excl,
+  #       "prix incl": price_incl,
+  #       "stock": stock,
+  #       "reviews": reviews
+  #     }
+  #     products.append(product)
 
 #def load_data(soup,products):
  # headers = ["upc","product type","prix excl","prix incl","stock","review"]
